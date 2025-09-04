f2707697385ac73d0c2964c395f399c6871e9cb8
AR-Wiley <ar.wiley805@outlook.com>
2025-09-03 20:14:10 -0500

    first commit

---
+++ ADUserAccountHelpers.ps1
@@ -0,0 +1,56 @@
+function isEnabled {
+
+    param (
+
+        [string]$UID
+
+    )
+
+    $enabled = Get-ADUser -Identity $UID -Properties Enabled
+
+    if (-not $enabled.Enabled) {
+
+        return $enabled.Enabled
+
+    } 
+}
+
+function isLocked {
+
+    param (
+
+        [string]$UID
+
+    )
+
+    $locked = Get-ADUser -Identity $UID -Properties LockedOut
+
+    if ($locked.LockedOut) {
+
+        Unlock-ADAccount -Identity $UID
+        return "Unlocked $UID"
+
+    } else {
+
+        return "$UID is not locked"
+    
+    }
+}
+
+function passwordReset {
+
+    param(
+
+        [string]$UID,
+        [SecureString]$Password
+
+    )
+
+    Set-ADAccountPassword -Identity $UID -NewPassword $Password -Reset
+
+    Set-ADUser -Identity $UID -ChangePasswordAtLogon $true
+
+}
+
+ 
+ 
